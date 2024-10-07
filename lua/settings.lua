ba940016996fb2be9c9ab60724de5fd874ac2d01
Ariel Kazula <arielkazula@users.noreply.github.com>
2024-10-07 13:19:11 +0300

    Update settings.lua
    added column limit

--- lua/settings.lua
+++ lua/settings.lua
@@ -6,4 +6,6 @@ vim.o.expandtab = true     -- Convert tabs to spaces
 vim.o.shiftwidth = 4       -- Indentation width
 vim.o.tabstop = 4          -- Tab width
 vim.o.clipboard = 'unnamedplus'
-
+vim.o.textwidth = 80
+vim.o.wrapmargin = 0
+vim.o.colorcolumn = "80"
