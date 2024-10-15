45bc5a1be698226f3b0e134e8d82ca67f8cfef06
Ariel Kazula <arielkazula@users.noreply.github.com>
2024-10-15 14:16:01 +0300

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,6 +9,15 @@ return {
 				ensure_installed = { "clangd" },
 				automatic_installation = true,
 			})
+			require('lspconfig').clangd.setup({
+              			capabilities = {
+                    			  offsetEncoding = { "utf-16" },
+                		},
+                		cmd = {
+                    			"clangd",
+                    			"--header-insertion=never",
+                		},
+            		})
 		end,
 	},
 
