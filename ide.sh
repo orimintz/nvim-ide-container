45700796deeaab295dfef45573e00673d63aabcc
Ariel Kazula <arielkazula@users.noreply.github.com>
2024-11-04 13:46:01 +0200

    Update ide.sh

--- ide.sh
+++ ide.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+# Set default value for WHITE_LIST_DOCKER if not already defined
+: "${WHITE_LIST_DOCKER:=ariel-ide}"
+
+# Export the variable to ensure it's available to the environment
+export WHITE_LIST_DOCKER
+
+# Your script's main commands go here
+echo "WHITE_LIST_DOCKER is set to $WHITE_LIST_DOCKER"
+
 docker exec -it ariel-ide bash
